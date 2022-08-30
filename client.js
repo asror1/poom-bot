ff85ef1a20eac2d9705a98c926bafcb41ca5f750
Asror <asrorklichev@yahoo.com>
2022-08-30 17:07:31 -0400

    Add kick users command

--- client.js
+++ client.js
@@ -2,11 +2,13 @@ const { Client, IntentsBitField, Partials } = require('discord.js');
 const clientOptions = {
     intents: [
         IntentsBitField.Flags.GuildMessages,
+	IntentsBitField.Flags.GuildMembers,
         IntentsBitField.Flags.Guilds,
         IntentsBitField.Flags.MessageContent
     ],
     partials: [
         Partials.User,
+	Partials.GuildMember,
         Partials.Message
     ]
 }
