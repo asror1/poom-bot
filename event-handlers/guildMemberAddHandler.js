5dd66fb26cd4591a19ea733c9004d454067a6a55
Asror <asrorklichev@yahoo.com>
2022-08-31 01:34:37 -0400

    Fix major bug with user join handler

--- event-handlers/guildMemberAddHandler.js
+++ event-handlers/guildMemberAddHandler.js
@@ -1,27 +1,33 @@
 const {rapid_api_key} = require('../config.json');
 const {default: axios} = require('axios');
-module.exports = async (member=undefined)=>{
+const defaultJokes = [
+    "Why is Peter Pan always flying? Because he Neverlands.",
+    "Which state has the most streets? Rhode Island.",
+    "What do you call 26 letters that went for a swim? Alphawetical.",
+    "What’s the name of a very polite, European body of water? Merci.",
+    "Why was the color green notoriously single? It was always so jaded.",
+    "I used to hate facial hair, but then it grew on me.",
+    "I want to make a brief joke, but it’s a little cheesy.",
+    "Why did the coach go to the bank? To get his quarterback.",
+    "How do celebrities stay cool? They have many fans."
+];
+const hooks = [
+    'Listen',
+    'Hear me out',
+    'Check this out',
+    'You gonna like this one',
+    'Escucha',
+    'Man',
+    'Dudeeee'
+];
+const greetings = [
+    'Hey', 'Howdy', 'Hola', 'Herro', 'Greetings', 'How do you do', 'Yo'
+];
+const emojis = [
+    '👀', '👅', '👊', '💥', '💯', '🖖', '😎', '😏', '😳', '😹'
+];
+module.exports = async (member)=>{
     if(!member) return;
-    const defaultJokes = [
-        "Why is Peter Pan always flying? Because he Neverlands.",
-        "Which state has the most streets? Rhode Island.",
-        "What do you call 26 letters that went for a swim? Alphawetical.",
-        "What’s the name of a very polite, European body of water? Merci.",
-        "Why was the color green notoriously single? It was always so jaded.",
-        "I used to hate facial hair, but then it grew on me.",
-        "I want to make a brief joke, but it’s a little cheesy.",
-        "Why did the coach go to the bank? To get his quarterback.",
-        "How do celebrities stay cool? They have many fans."
-    ];
-    const phrases = [
-        'Listen',
-        'Hear me out',
-        'Check this out',
-        'You gonna like this one',
-        'Escucha',
-        'Man',
-        'Dudeeee'
-    ];
     const url = 'https://geek-jokes.p.rapidapi.com/api?format=json';
     const options = {
         headers: {
@@ -31,9 +37,19 @@ module.exports = async (member=undefined)=>{
     };
     try {
         let response = await axios.get(url, options);
-        response = response.data;
-        member.guild?.channels.first().send(`Hey ${member.user.username}, ${phrases[Math.round(Math.random()* phrases.length)%phrases.length]}... ${response?.joke || defaultJokes[Math.round(Math.random()* defaultJokes.length)%defaultJokes.length]}`);
+        let joke = response?.data?.joke;
+        let channel = await member.guild?.channels.fetch();
+        channel = channel.find(elem=>elem.type===0);
+        channel.send(generateMadlib(member.user.id, joke));
     } catch(err){
         console.error(err);
     }
+}
+const randomIndex = (max) => Math.round(Math.random() * max) % max;
+const generateMadlib = (id, joke) =>{
+    let greeting = greetings[randomIndex(greetings.length)];
+    let hook = hooks[randomIndex(hooks.length)];
+    joke = joke || defaultJokes[randomIndex(defaultJokes.length)];
+    let emoji = emojis[randomIndex(emojis.length)];
+    return `${greeting} <@${id}>! ${hook}... ${joke} ${emoji}`;
 }
