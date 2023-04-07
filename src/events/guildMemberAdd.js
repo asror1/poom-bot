437dfa4150e36fc64daad6780cd75af2a569827b
Asror <asrorklichev@yahoo.com>
2023-04-07 16:12:51 -0400

    refactor: guildMemberAdd handler that was using scattered code

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,6 +1,8 @@
 const { Events, ChannelType } = require('discord.js')
-const greetMember = require('../util/greetMember')
-const fetchJoke = require('../api/fetchJoke')
+//const greetMember = require('../util/greetMember')
+const EnigmaBot = require('../util/EnigmaBot')
+
+const enigmaBot = new EnigmaBot();
 
 module.exports = {
     name: Events.GuildMemberAdd,
@@ -9,26 +11,6 @@ module.exports = {
             console.error("Member was not passed into guildMemberAdd handler");
             return
         }
-        try {
-            // ** Fetch nerdy jokes api for a joke ** 
-            let res = await fetchJoke()
-            joke = await res?.data?.joke
-
-            // Define variables needed for handler
-            let guild = member.guild,
-                channels = await guild.channels.fetch(),
-                channel
-            // ** Find the welcome channel **
-            channel = channels.find(elem => elem.type === ChannelType.GuildText && elem.name.includes("welcome"))
-            if (!channel) {
-                console.error(`No welcome channel found in ( ${guild.name} | ${guild.id} ) `);
-                console.log(`Using random channel to send welcome message for ( ${guild.name} | ${guild.id} )`);
-                channel = channels.find(elem => elem.type === ChannelType.GuildText)
-            }
-            channel.send(greetMember(member.user.id, joke));
-        }
-        catch (err) {
-            console.error(err);
-        }
+        enigmaBot.greet(member)
     }
 }
